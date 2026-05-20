73b509edcc9c008777a01d6ef194593e907459db
Anton Nikolskiy <freyas.kniv@gmail.com>
2026-05-20 17:30:45 +0300

    ДЗ #1

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -2,14 +2,48 @@ package main
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
 
-func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+const ByteSize = 8
+const ByteMask = 0xFF
+
+type Number interface {
+	uint16 | uint32 | uint64
+}
+
+// GetByte получает определенный байт по индексу
+func GetByte[T Number](number T, index int) uint8 {
+	return uint8(number >> (index * ByteSize) & ByteMask) // сдвигаем на index*8 и отрезаем остальное
+}
+
+// ToLittleEndian смена порядка байт Big -> Little Endian.
+func ToLittleEndian[T Number](number T) T {
+	var result T
+	bytesAmount := int(unsafe.Sizeof(number))
+
+	for index := range bytesAmount {
+		// Берем index байт из числа
+		current := T(GetByte(number, index))
+
+		/*
+			Вычисляем смещение для результата в битах - инверсная логика, первый бит окажется последним.
+			Используем OR - в нужной нам позиции стоит 0 (null value), OR подставит туда значение.
+			Например, для первой итерации для number = 0x12345678:
+				- shift = (4 - 1 - 0) * 8 = 24 (самая старшая позиция)
+				- current байт = 0x78
+				- result = 0x00000000 | 0x78000000 = 0x78000000
+		*/
+
+		shift := (bytesAmount - 1 - index) * ByteSize
+		result |= current << shift
+	}
+
+	return result
 }
 
 func TestСonversion(t *testing.T) {
