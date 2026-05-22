c09ed5db08d0331c6bac33ac5d76bf858d3f0699
Anton Nikolskiy <freyas.kniv@gmail.com>
2026-05-22 10:29:11 +0300

    Добавил тесты для других типов

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -46,7 +46,38 @@ func ToLittleEndian[T Number](number T) T {
 	return result
 }
 
-func TestСonversion(t *testing.T) {
+func TestUint16(t *testing.T) {
+	tests := map[string]struct {
+		number uint16
+		result uint16
+	}{
+		"test case #1": {
+			number: 0x0000,
+			result: 0x0000,
+		},
+		"test case #2": {
+			number: 0xFFFF,
+			result: 0xFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF,
+			result: 0xFF00,
+		},
+		"test case #4": {
+			number: 0x0102,
+			result: 0x0201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestUint32(t *testing.T) {
 	tests := map[string]struct {
 		number uint32
 		result uint32
@@ -70,6 +101,45 @@ func TestСonversion(t *testing.T) {
 		"test case #5": {
 			number: 0x01020304,
 			result: 0x04030201,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := ToLittleEndian(test.number)
+			assert.Equal(t, test.result, result)
+		})
+	}
+}
+
+func TestUint64(t *testing.T) {
+	tests := map[string]struct {
+		number uint64
+		result uint64
+	}{
+		"test case #1": {
+			number: 0x0000000000000000,
+			result: 0x0000000000000000,
+		},
+		"test case #2": {
+			number: 0xFFFFFFFFFFFFFFFF,
+			result: 0xFFFFFFFFFFFFFFFF,
+		},
+		"test case #3": {
+			number: 0x00FF00FF00FF00FF,
+			result: 0xFF00FF00FF00FF00,
+		},
+		"test case #4": {
+			number: 0x00000000FFFFFFFF,
+			result: 0xFFFFFFFF00000000,
+		},
+		"test case #5": {
+			number: 0x0102030405060708,
+			result: 0x0807060504030201,
+		},
+		"test case #6": {
+			number: 0x1234567890ABCDEF,
+			result: 0xEFCDAB9078563412,
 		},
 	}
 
